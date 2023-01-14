ab48eef2ed891b7929f9b00eb2aebb21b8ae3aff
Autsada T <autsada@gmail.com>
2023-01-14 20:39:49 +0700

    handle cancel modal case

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,7 +2,8 @@ import React from "react"
 import type { LoaderArgs, ActionArgs } from "@remix-run/node"
 import { json } from "@remix-run/node"
 import { useLoaderData, useFetcher } from "@remix-run/react"
-import { useAuthenticityToken, useHydrated } from "remix-utils"
+import { useAuthenticityToken, useHydrated, ClientOnly } from "remix-utils"
+import { useWeb3Modal } from "@web3modal/react"
 import { useAccount, useDisconnect } from "wagmi"
 
 import {
@@ -41,10 +42,11 @@ export default function Dashboard() {
   const hydrated = useHydrated()
 
   const [processingLogin, setProcessingLogin] = React.useState(false)
-  const [error, setError] = React.useState("")
+  const [logInError, setLogInError] = React.useState("")
 
-  const { address, isConnected } = useAccount()
+  const { address, isConnected, isConnecting } = useAccount()
   const { disconnect } = useDisconnect()
+  const { isOpen } = useWeb3Modal()
 
   // When users connected their wallet, submit the address to the server for processing Firebase Auth login
   React.useEffect(() => {
@@ -63,7 +65,7 @@ export default function Dashboard() {
         // Send the `idToken` and `csrf` token to the `action` function on the server.
         fetcher.submit({ idToken, csrf }, { method: "post", action: "/login" })
       } catch (error) {
-        setError("Error occurred while attempting to login")
+        setLogInError("Error occurred while attempting to login")
         setProcessingLogin(false)
       }
     },
@@ -71,12 +73,19 @@ export default function Dashboard() {
     [csrf]
   )
 
+  // When we get the token in the browser, log user in
   React.useEffect(() => {
     if (token) {
       login(token)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [token])
+
+  React.useEffect(() => {
+    if (!isOpen && isConnecting) {
+      setProcessingLogin(false)
+    }
+  }, [isConnecting, isOpen])
 
   function logOut() {
     // Make sure to reset the processing state
@@ -86,21 +95,24 @@ export default function Dashboard() {
     fetcher.submit({}, { method: "post", action: "/logout" })
   }
 
-  console.log("error -->", error)
   return (
-    <div>
-      {data.user ? (
-        <div className="mt-6">
-          <h3 className="text-3xl">Dashboard</h3>
-          <LogOutButton logOut={logOut} disabled={!hydrated} />
+    <ClientOnly>
+      {() => (
+        <div>
+          {data.user ? (
+            <div className="mt-6">
+              <h3 className="text-3xl">Dashboard</h3>
+              <LogOutButton logOut={logOut} disabled={!hydrated} />
+            </div>
+          ) : (
+            <ConnectButton
+              processing={processingLogin}
+              setProcessing={setProcessingLogin}
+              disabled={!hydrated || processingLogin}
+            />
+          )}
         </div>
-      ) : (
-        <ConnectButton
-          processing={processingLogin}
-          setProcessing={setProcessingLogin}
-          disabled={!hydrated || processingLogin}
-        />
       )}
-    </div>
+    </ClientOnly>
   )
 }
